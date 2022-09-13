dc4a95defc95b7c9d099f2fb8e831d2376a71cc5
Lucas Rodrigues <lukinhasr1423@gmail.com>
2022-09-13 12:23:37 -0300

    Adicionado a funcionalidade de add tarefas

---
+++ js/script.js
@@ -0,0 +1,72 @@
+let inputNovaTarefa = document.querySelector('#input-nova-tarefa');
+let btnAddTarefa = document.querySelector('#btn-add-tarefa');
+let listaTarefas = document.querySelector('#lista-tarefas');
+
+inputNovaTarefa.addEventListener('keypress', (e) => {
+
+    if (e.keyCode == 13) {
+        let tarefa = {
+            nome: inputNovaTarefa.value,
+            id: gerarId(),
+        }
+        adicionarTarefa(tarefa);
+    }
+});
+
+btnAddTarefa.addEventListener('click', (e) => {
+
+    let tarefa = {
+        nome: inputNovaTarefa.value,
+        id: gerarId(),
+    }
+    adicionarTarefa(tarefa);
+});
+
+function gerarId() {
+    return Math.floor(Math.random() * 3000);
+}
+
+function adicionarTarefa(tarefa) {
+
+    let li = criarTagLi(tarefa);
+    listaTarefas.appendChild(li);
+    inputNovaTarefa.value = '';
+
+}
+
+function criarTagLi(tarefa) {
+
+    let li = document.createElement('li');
+
+    let span = document.createElement('span');
+    span.classList.add('texto-tarefa');
+    span.innerHTML = tarefa.nome;
+
+    let div = document.createElement('div');
+
+    let btnEditar = document.createElement('button');
+    btnEditar.classList.add('btn-acao');
+    btnEditar.innerHTML = '<i class="fa-solid fa-pen"></i>';
+    btnEditar.setAttribute('onclick', 'editar('+tarefa.id+')');
+
+    let btnExcluir = document.createElement('button');
+    btnExcluir.classList.add('btn-acao');
+    btnExcluir.innerHTML = '<i class="fa-solid fa-trash"></i>';
+    btnExcluir.setAttribute('onclick', 'excluir('+tarefa.id+')');
+
+    div.appendChild(btnEditar);
+    div.appendChild(btnExcluir);
+
+    li.appendChild(span);
+    li.appendChild(div);
+
+    return li;
+}
+
+function editar(idTarefa) {
+    alert(idTarefa);
+}
+
+function excluir(idTarefa) {
+    alert(idTarefa);
+}
